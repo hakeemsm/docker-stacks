d4cbeba91c1a8849dce507cf86ae0c4427aa13b2
Hakeem Mohammad <hakeemosrc@gmail.com>
2018-05-17 14:27:00 -0400

    changed port to 443 and updated to use tf 1.8

--- base-notebook/jupyter_notebook_config.py
+++ base-notebook/jupyter_notebook_config.py
@@ -8,8 +8,8 @@ import errno
 import stat
 
 c = get_config()
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.port = 8888
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = 443
 c.NotebookApp.open_browser = False
 
 # https://github.com/jupyter/notebook/issues/3130
